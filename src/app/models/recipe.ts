7c180e67ac0a84449fd294fe389ed3306b8cb00e
Mohammad abbas <mohammed3bbas99@gmail.com>
2021-10-04 18:24:36 +0300

    recipe service

---
+++ src/app/models/recipe.ts
@@ -0,0 +1,7 @@
+export interface Recipe{
+    id : number,
+    name : string,
+    minutes : number,
+    imageUrl : string,
+    categoryId : number
+}
---
+++ src/app/services/recipe.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Recipe } from '../models/recipe';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class RecipeService {
+  private apiServerUrl = '';
+
+  constructor(private http: HttpClient) { }
+  // GETS 
+  public getAllRecipes(): Observable<Recipe[]> {
+    return this.http.get<Recipe[]>(`${this.apiServerUrl}/recipes`);
+  }
+
+  public getRecipeById(id: number): Observable<Recipe> {
+    return this.http.get<Recipe>(`${this.apiServerUrl}/recipes/${id}`);
+  }
+
+
+
+  public getRecipesByName(name: string): Observable<Recipe[]> {
+    return this.http.get<Recipe[]>(`${this.apiServerUrl}/recipes/${name}`);
+  }
+
+  public getRecipesByCategoryName(name: string): Observable<Recipe[]> {
+    return this.http.get<Recipe[]>(`${this.apiServerUrl}/recipes/category?name=${name}`);
+  }
+  // POST
+  public addRecipe(recipe : Recipe) : Observable<Recipe>{
+    return this.http.post<Recipe>(`${this.apiServerUrl}/recipes`,recipe);
+  }
+
+  //PUT 
+  public updateRecipe(recipe : Recipe) : Observable<Recipe>{
+    return this.http.put<Recipe>(`${this.apiServerUrl}/recipes`,recipe);
+  }
+
+  //DELETE
+
+  public deleteRecipe(id : number) : Observable<void>{
+    return this.http.delete<void>(`${this.apiServerUrl}/recipes/${id}`);
+  }
+
+
+
+
+}
